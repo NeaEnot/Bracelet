95774fcf2927bd01e7a9d5ce880f566e607c9dca
NeaEnot <oleg.kozloss@yandex.ru>
2020-05-13 22:02:25 +0400

    ver 1.1

---
+++ FileLogic.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Text;
+
+namespace Bracelet
+{
+    static class FileLogic
+    {
+        static public (string leftArm, string rightArm) getArms(int count, bool isRight)
+        {
+            List<string> bracelets = read(ConfigurationManager.AppSettings["path"]);
+            var answer = generate(bracelets, count, isRight);
+
+            return answer;
+        }
+
+        static private List<string> read(string path)
+        {
+            List<string> answer = new List<string>();
+
+            using (var reader = new StreamReader(path, Encoding.UTF8))
+            {
+                while (!reader.EndOfStream)
+                {
+                    answer.Add(reader.ReadLine());
+                }
+            }
+
+            return answer;
+        }
+
+        static private (string, string) generate(List<string> bracelets, int count, bool isRight)
+        {
+            string leftArm = "";
+            string rightArm = "";
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < count; i++)
+            {
+                int number = rnd.Next(0, bracelets.Count - 1);
+
+                string bracelet = bracelets[number];
+                bracelets.RemoveAt(number);
+
+                if (!isRight || rnd.Next(0, 4) > 0)
+                {
+                    leftArm += bracelet + "\n";
+                }
+                else
+                {
+                    rightArm += bracelet + "\n";
+                }
+            }
+
+            return (leftArm, rightArm);
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Configuration;
+using System.Windows;
+
+namespace Bracelet
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Random rnd = new Random();
+
+            int count = textBox.Text.Length > 0 ? int.Parse(textBox.Text) : rnd.Next(1, int.Parse(ConfigurationManager.AppSettings["maxRandom"]));
+            bool isRight = checkBoxRightArm.IsChecked.Value;
+
+            var arms = FileLogic.getArms(count, isRight);
+
+            textBlockLeftArm.Text = arms.leftArm;
+            textBlockRightArm.Text = arms.rightArm;
+        }
+    }
+}
